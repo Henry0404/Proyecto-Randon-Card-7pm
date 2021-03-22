9f18ec98cd3456677f9461d6af5afce40cccb8f4
Henry Romero <hromerp22@gmail.com>
2021-03-22 01:22:19 +0100

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
+/* /* eslint-disable */
+/* import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico"; */
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var numeroAleatorio = Math.floor(Math.random() * 4);
+  let simbolos = ["♦", "♥", "♠", "♣"]
+  var simbolo = simbolos[numeroAleatorio]
+  var iconos = document.getElementsByClassName('icono')
+
+  for (let index = 0; index < iconos.length; index++) {
+    const icono = iconos[index];
+    icono.innerHTML = simbolo;
+  }
+
+  var numeroDelMedio = Math.floor(Math.random() * 12) +1;
+
+  document.getElementById("numero").innerHTML = numeroDelMedio;
 };
+
+
+
